dcd8700a7902a65e6d1733578f87258d8433a10f
Hari Anugrah <code.anugrah@gmail.com>
2021-05-11 23:55:42 +0800

    a bit of layouting

--- app/src/main/java/id/aryad/sipasar/BayarGajiActivity.java
+++ app/src/main/java/id/aryad/sipasar/BayarGajiActivity.java
@@ -55,55 +55,5 @@ public class BayarGajiActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        activityTitle = (TextView) findViewById(R.id.title);
-        updateSelectedMonth();
-
-        btnSelectRange = (Button) findViewById(R.id.btnSelectRange);
-        btnSelectRange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerDialog dialog = createDialogWithoutDateField();
-                dialog.show();
-            }
-        });
-    }
-
-    private DatePickerDialog createDialogWithoutDateField() {
-        DatePickerDialog dpd = new DatePickerDialog(this, null, 2014, 1, 24);
-        try {
-            java.lang.reflect.Field[] datePickerDialogFields = dpd.getClass().getDeclaredFields();
-            for (java.lang.reflect.Field datePickerDialogField : datePickerDialogFields) {
-                if (datePickerDialogField.getName().equals("mDatePicker")) {
-                    datePickerDialogField.setAccessible(true);
-                    DatePicker datePicker = (DatePicker) datePickerDialogField.get(dpd);
-                    java.lang.reflect.Field[] datePickerFields = datePickerDialogField.getType().getDeclaredFields();
-                    for (java.lang.reflect.Field datePickerField : datePickerFields) {
-//                        Log.i("test", datePickerField.getName());
-                        if ("mDaySpinner".equals(datePickerField.getName())) {
-                            datePickerField.setAccessible(true);
-                            Object dayPicker = datePickerField.get(datePicker);
-                            ((View) dayPicker).setVisibility(View.GONE);
-                        }
-                    }
-                }
-            }
-        } catch (Exception ex) {
-        }
-        return dpd;
-    }
-
-    void updateSelectedMonth() {
-        String month_name = (String)android.text.format.DateFormat.format("MMMM", new Date());
-//        int id_admin = getIntent().getIntExtra(IntentKey.CURRENT_ADMIN_ID, -1);
-//        String username;
-//        if (id_admin != -1) {
-//            username = AuthRepository.getInstance().getAdminById(id_admin).getUsername();
-//        } else {
-//            username = "Gagal Login";
-//        }
-//        String username = AuthRepository.getInstance().getCurrentAdmin(getApplicationContext()).getUsername();
-
-//        activityTitle.setText("Gaji " + month_name + ", " + String.valueOf(selectedYear) + " (" + username + ")");
     }
 }
